f0bbb0a955c06fae18fa63d4c4b4729aed93e1d7
fanuman <numanfarukh22@gmail.com>
2021-03-01 15:48:09 +0500

    working on header section

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton'
+
+function Header() {
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon"/>
+            </IconButton>
+            <img
+                src=""
+            ></img>
+        </div>
+    )
+}
+
+export default Header
